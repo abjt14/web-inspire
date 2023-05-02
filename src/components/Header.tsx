727677050c22fa4654f56c95a70e15711495186b
Abhijeet Singh <abhijeetsinghmain@gmail.com>
2023-05-01 19:49:37 -0700

    fixed header horizontal padding issue in responsive sizes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function _() {
   return (
     <header className="w-full border-b border-neutral-700">
-      <div className="flex gap-4 justify-between items-center text-xl py-8 px-8 max-w-screen-2xl mx-auto">
+      <div className="flex gap-4 justify-between items-center text-xl py-8 px-8 sm:py-4 sm:px-4 lg:py-8 lg:px-8 max-w-screen-2xl mx-auto">
         <section className="flex flex-col gap-2 justify-center items-start">
-          <Link href="/" className="text-neutral-50 text-base sm:text-3xl">Web Inspirations</Link>
+          <Link href="/" className="text-neutral-50 text-base sm:text-3xl">Web Inspire</Link>
           <p className="text-neutral-500 text-sm hidden sm:block">A curated list of good-looking websites.</p>
         </section>
         <section>
